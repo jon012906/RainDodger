4ca5566cc275a1cce970298eddb1bec448f038fc
Jonathan Limanza <jonathanlimanza@gmail.com>
2026-08-25 09:55:42 +0700

    Initial Commit

---
+++ RainDodger/RainDodgerApp.swift
@@ -0,0 +1,32 @@
+//
+//  RainDodgerApp.swift
+//  RainDodger
+//
+//  Created by Jon on 25/08/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct RainDodgerApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
